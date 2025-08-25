45b8e8b11f3315feb6a1ea24846744fd2ac41167
eshankulasekara <eshankulasekara@gmail.com>
2025-08-25 16:42:13 +0530

    admin dashboard findUser 1

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,16 @@
+import Form from 'next/form';
+
 export default function AdminPage(){
     return(
         <div className="flex items-center justify-center h-screen">
             <h1>This page is for Admins only</h1>
+            <div>
+                <Form action={findUser}>
+                    <input name="first_name" type="text" placeholder="First Name" className="input input-bordered" />
+                    <input name="last_name" type="text" placeholder="Last Name" className="input input-bordered" />
+                    <button type="submit" className="btn btn-primary">Find User</button>
+                </Form>
+            </div>
         </div>
     )
 }
--- src/app/api/getUsers.ts
+++ src/app/api/getUsers.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
     // This is a placeholder for the getUsers API endpoint
     // You can implement your logic to fetch users here
     return new Response("This is the getUsers API endpoint", { status: 200 });
